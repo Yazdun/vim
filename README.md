c612e59af6396093754a3c03e8ad67eeaaafdbb9
Yazdun <yazduntube@gmail.com>
2022-06-02 11:56:00 +0430

    commit

---
+++ README.md
@@ -0,0 +1,19 @@
+| Action                           |    Key     |
+| -------------------------------- | :--------: |
+| up & down                        |   K / J    |
+| skip code brackets               | shift + [  |
+| select mode                      | shift + v  |
+| make a selection                 |  ctrl + v  |
+| delete single character          |     x      |
+| similar to ctrl z                |     u      |
+| redo                             |  ctrl + r  |
+| to the bottom                    | shift + g  |
+| to the top                       |   g + g    |
+| see the end of parantes          | shift + 5  |
+| delete and copy to clipboard     |     d      |
+| paste                            |     p      |
+| next instance of the word        | shift + \* |
+| go to the next word              |     w      |
+| insert mode at start of the line | shift + a  |
+| skip the available spaces        |     w      |
+| go to the start of the line      |  shift 6   |
---
+++ index.js
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import css from "./styles.module.css";
+import { motion } from "framer-motion";
+import { usePlanet } from "../../hooks";
+import { pathnames } from "./data";
+
+export const MobileTabs = ({ planet = "earth", color = "#6D2ED5" }) => {
+  const { currentTab } = usePlanet();
+  const activeLinkAriaLabel = (name) =>
+    name === currentTab ? `${name} is the current tab` : name;
+
+  return (
+    <nav className={css.nav}>
+      <ul className={css.list}>
+        {pathnames.map((item) => {
+          const { path } = item;
+          return (
+            <li className={css.item} key={path}>
+              <Link
+                to={`/${planet}/${path}`}
+                aria-label={activeLinkAriaLabel(path)}
+              >
+                {path}
+              </Link>
+              <Underline path={path} color={color} />
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+const Underline = ({ path, color }) => {
+  const { currentTab } = usePlanet();
+  return (
+    <motion.div
+      initial={{ scaleX: "0" }}
+      animate={{ scaleX: currentTab === path ? 1 : 0 }}
+      exit={{ scaleX: 0 }}
+      transition={{ duration: 0.3 }}
+      className={css.progress}
+      style={{ backgroundColor: color }}
+    ></motion.div>
+  );
+};
